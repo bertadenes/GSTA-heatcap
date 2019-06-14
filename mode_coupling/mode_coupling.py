nosymm fix, coefficient calculation done

mode_coupling/mode_coupling.py
@@ -2,6 +2,9 @@ import os
 import numpy as np
 import modules.g09BOMD_filter as g
 import modules.GetHeatcapacity_util as u
+import matplotlib
+matplotlib.use('TkAgg') # MUST BE CALLED BEFORE IMPORTING plt
+import matplotlib.pyplot as plt
 
 
 class Namespace:
@@ -11,6 +14,7 @@ class Namespace:
 
 def coupling_process(freqname):
     freq = g.processg09output(freqname)
+    zero_pot = freq.scfenergies[-1] * 0.036749324
     MDname = os.path.join(os.path.abspath(os.path.dirname(freqname)), "MD",
                           "{0:s}_traj{1:d}_MD_pos.out".format(freqname.split(os.sep)[-1].split('.')[0], 1))
     traj = Namespace()
@@ -19,9 +23,33 @@ def coupling_process(freqname):
     traj.Epot = g.getEpot(MDname)[0, :]
     traj.Ekin = np.mean(np.array(u.calcEkin(traj.vel)), axis=(0, 1))[:, 0]
     traj.vel = traj.vel[0, :, :, :]
+    modecoeff = np.empty(shape=(len(traj.Ekin), len(freq.vibfreqs)), dtype=np.float_)
+    kincoeff = np.empty(shape=(len(traj.Ekin), len(freq.vibfreqs)), dtype=np.float_)
+    potcoeff = np.empty(shape=(len(traj.Ekin), len(freq.vibfreqs)), dtype=np.float_)
     for i in range(len(traj.Ekin)):
-        print("{0:f} {1:f} {2:f}".format(traj.Ekin[i],
-                                         traj.Epot[i], #freq.scfenergies[-1]) * 0.036749324,
-                                         traj.Ekin[i]+traj.Epot[i]))
+        # print("{0:f} {1:f} {2:f}".format(traj.Ekin[i],
+        #                                  traj.Epot[i] - zero_pot,
+        #                                  traj.Ekin[i]+traj.Epot[i]))
+        disp = np.reshape((traj.coord[i] - freq.atomcoords[-1]), freq.natom * 3)
+        proj_pot = []
+        proj_kin = []
+        for v in freq.vibdisps:
+            proj_pot.append(np.abs(np.dot(np.reshape(v, freq.natom * 3) / np.linalg.norm(np.reshape(v, freq.natom * 3)), disp)))
+            proj_kin.append(np.abs(np.dot(np.reshape(v, freq.natom * 3) / np.linalg.norm(np.reshape(v, freq.natom * 3)), np.reshape(traj.vel[i], freq.natom * 3))))
+        kinpart = traj.Ekin[i] / (traj.Ekin[i] + traj.Epot[i] - zero_pot)
+        potpart = 1 - kinpart
+        for j in range(len(freq.vibdisps)):
+            kincoeff[i][j] = proj_kin[j] / np.sum(proj_kin)
+            potcoeff[i][j] = proj_pot[j] / np.sum(proj_pot)
+            modecoeff[i][j] = kinpart * kincoeff[i][j] + potpart * potcoeff[i][j]
+        print(i+1, "done")
+    f, ax = plt.subplots()
+    plt.xlabel("Step")
+    plt.ylabel("Coefficient")
+    ax.plot(modecoeff[:, 57], label="total")
+    ax.plot(kincoeff[:, 57], label="kinetic")
+    ax.plot(potcoeff[:, 57], label="potential")
+    plt.legend(loc='upper left')
+    plt.show()
 
     print("stop")

modules/GetHeatcapacity_util.py
@@ -105,7 +105,7 @@ def calcEkin(mwvel):
         #    ek[k] = j ** 2 * 0.5
         #    k += 1
         # ek = np.divide(np.square(i), 2)
-        ek = np.divide(np.square(i), 72 * 2 * 9.375828402564380E+29)  # in hartrees
+        ek = np.divide(72 * np.square(i), 2 * 9.375828402564380E+29)  # in hartrees
         ekin.append(ek)
         # ek = np.divide(np.sum(np.square(i)),2*9.375828402564380E+29) # in hartrees
         # ekin[l] = ek

modules/g09BOMD_filter.py
@@ -409,7 +409,7 @@ def createMoveInputs(Calc):
         # rndtemp = genRndTemp(Calc.temp, Calc.mol.natom)
         rndtemp = Calc.temp
         com = " BOMD(update=1000,StepSize=1,MaxPoints=1,nsample=" + str(
-            len(Calc.mol.vibfreqs)) + ",NTraj=1,Sample=Microcanonical,rtemp=0) IOp(1/44=" + str(rand) + ")"
+            len(Calc.mol.vibfreqs)) + ",NTraj=1,Sample=Microcanonical,rtemp=0) IOp(1/44=" + str(rand) + ")  nosymmetry"
         title = "Trajectory " + str(i + 1) + " for " + Calc.freqfile.split('.')[0]
         energy = np.float64(const.R * (rndtemp) * 0.000239005736137668)
         Calc.addSubCalc(inp)
@@ -533,7 +533,7 @@ def createVelInputs(Calc):
         rndtemp = genRndTemp(Calc.temp, Calc.mol.natom)
         rtemp = str(int(rndtemp))
         com = " BOMD(update=1000,StepSize=1,MaxPoints=1,nsample=" + str(
-            len(Calc.mol.vibfreqs)) + ",NTraj=1,Sample=Microcanonical,rtemp=" + rtemp + ") IOp(1/44=" + str(rand) + ")"
+            len(Calc.mol.vibfreqs)) + ",NTraj=1,Sample=Microcanonical,rtemp=" + rtemp + ") IOp(1/44=" + str(rand) + ") nosymmetry"
         title = "Trajectory " + str(i + 1) + " for " + Calc.freqfile.split('.')[0]
         energy = np.float64(int(rndtemp) * 0.001987203611)
         Calc.addSubCalc(inp)